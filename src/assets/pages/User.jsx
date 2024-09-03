dfefad706b3d7fa1c80b8ff34d7ba301522b2b1a
bennabbou <bennabbou.a@gmail.com>
2024-09-03 16:55:14 +0200

    creation et ajout des composants BankAccount dans User

--- src/assets/pages/User.jsx
+++ src/assets/pages/User.jsx
@@ -11,26 +11,9 @@ function User() {
     <h2 className="sr-only">Accounts</h2>
    
     <BankAccount title="Argent Bank Checking (x8349)" amount="$2,082.79" info="Available Balance" />
-    <section className="account">
-      <div className="account-content-wrapper">
-        <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-        <p className="account-amount">$10,928.42</p>
-        <p className="account-amount-description">Available Balance</p>
-      </div>
-      <div className="account-content-wrapper cta">
-        <button className="transaction-button">View transactions</button>
-      </div>
-    </section>
-    <section className="account">
-      <div className="account-content-wrapper">
-        <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-        <p className="account-amount">$184.30</p>
-        <p className="account-amount-description">Current Balance</p>
-      </div>
-      <div className="account-content-wrapper cta">
-        <button className="transaction-button">View transactions</button>
-      </div>
-    </section>
+    <BankAccount title="Argent Bank Savings (x6712)" amount="$10,928.42" info="Available Balance" />
+    <BankAccount title="Argent Bank Credit Card (x8349)" amount="$184.30" info="Current Balance" />
+    
   </main>
   )
 }
