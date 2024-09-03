e32bb8c74208c4ffe150a222360b1eac245ea64d
bennabbou <bennabbou.a@gmail.com>
2024-09-03 16:37:01 +0200

    creation de router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import Home from './assets/pages/Home';
+import SingIn from './assets/pages/SingIn';
+import User from './assets/pages/User';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/signin",
+        element: <SingIn />,
+      },
+      {
+        path: "/user",
+        element: <User />,
+      }
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} /> 
   </StrictMode>,
-)
+);
