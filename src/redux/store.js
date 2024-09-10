bee54ab302e41df726343dcedd10e874e403ad15
bennabbou <bennabbou.a@gmail.com>
2024-09-11 00:20:54 +0200

    ajout de userSlice sur store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit"; // Importer la fonction configureStore depuis la bibliothèque Redux Toolkit pour configurer le store
-import userReducer from './UserSlice'; 
-
+import authReducer from './authSlice'; 
+import userSlice from "./userSlice";
 
 // Configurer le store avec configureStore
 const store = configureStore({
   reducer: {                   // objet qui contient la clé reducer, qui regroupe tous les reducers utilisés
-    user: userReducer,         // définit une partie de l'état appelée "user" qui est gérée par le réducteur userReducer
+    auth: authReducer,         // définit une partie de l'état appelée "user" qui est gérée par le réducteur userReducer
+    user: userSlice,
   },
 });
 
