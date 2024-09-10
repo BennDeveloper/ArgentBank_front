ee679e6f01f9e7ba82e037f2552fccbf7626da40
bennabbou <bennabbou.a@gmail.com>
2024-09-11 00:24:28 +0200

    ajout linput de user

--- src/assets/pages/User.jsx
+++ src/assets/pages/User.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import BankAccount from '../components/BankAccount'
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { getUserData, onIsEditing } from '../../redux/userSlice';
 
 function User() {
+
+  const { firstName, lastName, userName, loading, error, isEditing } = useSelector((state) => state.user);
+  const { token } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!token) {  //si un token est présent
+      navigate('/signin');  
+    } else {
+      dispatch(getUserData(token))
+    }
+  }, [token, navigate]);
+
   return (
     <main className="main bg-dark">
-    <div className="header">
-      <h1>Welcome back<br />Tony Jarvis!</h1>
-      <button className="edit-button">Edit Name</button>
-    </div>
+    {
+    !isEditing ? <div className="header">
+      <h1>Welcome back<br />{firstName} {lastName}!</h1>
+      <button className="edit-button" onClick={() => dispatch(onIsEditing())}>Edit Name</button>
+    </div> 
+    :
+    <form>
+       <input type="text" value={userName} />
+      <input type="text" disabled value={firstName} />
+      <input type="text" disabled value={lastName} />
+      <button className="edit-button" onClick={() => dispatch(onIsEditing())}>Cancel</button>
+    </form>
+    }
     <h2 className="sr-only">Accounts</h2>
    
     <BankAccount title="Argent Bank Checking (x8349)" amount="$2,082.79" info="Available Balance" />
